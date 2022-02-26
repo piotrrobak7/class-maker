8f61f62139e9b7471f3b5f3d1be09ed59573b95e
piter <piotrrobak7@gmail.com>
2022-02-26 20:16:40 +0100

    Project setup

---
+++ ClassMaker/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ClassMaker
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # class-maker
+
+This going to be a simple command line tool helping with generation of simple classes in VS.
